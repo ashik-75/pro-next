58bb722c23ded5f8d992c7e83fe7ef17818bf949
ashik-75 <ashikex49@gmail.com>
2024-01-16 02:07:19 +0600

    pp

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ import React from "react";
 const Page = () => {
   return (
     <div>
-      <Link href={"/store"}>Store</Link>
+      <Link href={"/store"}>Visit Store</Link>
     </div>
   );
 };
